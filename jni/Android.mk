57a32dee0b363e5651257f2a15f174ff1d276247
AngelDE98 <nuda1998@gmail.com>
2015-11-06 21:54:07 +0100

    Add MojoShader submodule and add it to Android.mk
    android-cmake hates me

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+TARGET_PLATFORM := 18
+LOCAL_MODULE    := mojoshader
+LOCAL_SRC_FILES := mojoshader/mojoshader.c mojoshader/mojoshader_common.c mojoshader/mojoshader_effects.c mojoshader/mojoshader_opengl.c
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv2 -Wl,--export-dynamic -lm -ldl
+LOCAL_CFLAGS += -Ijni/mojoshader/GL -DMOJOSHADER_NO_VERSION_INCLUDE
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := monosgen-2.0
 LOCAL_SRC_FILES := libs/libmonosgen-2.0.so
 include $(PREBUILT_SHARED_LIBRARY)
@@ -30,4 +38,4 @@ LOCAL_SHARED_LIBRARIES := SDL2 monosgen-2.0
 LOCAL_CFLAGS += -I/sdcard/Android/data/com.angelde.fnadroid/mono/include/mono-2.0 -I../sdl/include -Ijni/include -D_REENTRANT
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,android/native_app_glue) 
+$(call import-module,android/native_app_glue)
